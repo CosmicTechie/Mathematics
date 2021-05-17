19006aba2ec19d5c529667d225b0a86889401e89
Nishant Kumar <62173480+CosmicTechie@users.noreply.github.com>
2021-05-17 13:37:02 +0530

    Update Permutations_Combinations.py

--- Statistics and Probability/Permutations_Combinations.py
+++ Statistics and Probability/Permutations_Combinations.py
@@ -8,5 +8,11 @@ print("Combinations",len(list(combinations(arr,2))))
 print(list(permutations(arr,2)))
 print("Permutations",len(list(permutations(arr,2))))
 
-
+#Output
+'''
+[('H', 'O'), ('H', 'R'), ('H', 'S'), ('H', 'E'), ('O', 'R'), ('O', 'S'), ('O', 'E'), ('R', 'S'), ('R', 'E'), ('S', 'E')]
+Combinations 10
+[('H', 'O'), ('H', 'R'), ('H', 'S'), ('H', 'E'), ('O', 'H'), ('O', 'R'), ('O', 'S'), ('O', 'E'), ('R', 'H'), ('R', 'O'), ('R', 'S'), ('R', 'E'), ('S', 'H'), ('S', 'O'), ('S', 'R'), ('S', 'E'), ('E', 'H'), ('E', 'O'), ('E', 'R'), ('E', 'S')]
+Permutations 20
+'''
 
